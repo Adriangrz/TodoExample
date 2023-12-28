948ffd7e8166c12d3006a7284e1ebcb379655e27
Adriangrz <adriang1412g@gmail.com>
2023-12-28 09:49:50 +0100

    Add entity and repository interface

---
+++ TodoExample/TodoExample.Core/Entities/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoExample.Core.Entities
+{
+	public class Todo
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+	}
+}
+
---
+++ TodoExample/TodoExample.Core/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace TodoExample.Core.Interfaces
+{
+	public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> List();
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+    }
+}
+
